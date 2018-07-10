714f4e6015ed48553be92751d82c521c0451c52e
Mercedes Y Thompson <mythompson@bsu.edu>
2018-07-10 16:11:01 -0400

    use pokemon instead

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 
 import './Github.css'
+import GithubUser from './GithubUser'
 
 class Github extends Component {
+  state = {
+    username: '',
+  }
+
+  handleChange = ev => {
+    this.setState({ username: ev.target.value })
+  }
+
+  handleSubmit = ev => {
+    ev.preventDefault()
+    this.props.history.push(`/github/${this.state.username}`)
+  }
+
   render() {
     return (
       <div className="Github">
@@ -12,12 +27,14 @@ class Github extends Component {
           className="logo"
         />
 
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div>
             <input
               required
               autoFocus
               type="text"
+              value={this.state.username}
+              onChange={this.handleChange}
             />
           </div>
           <div>
@@ -26,6 +43,11 @@ class Github extends Component {
             </button>
           </div>
         </form>
+
+        <Route
+          path="/github/:username"
+          component={GithubUser}
+        />
       </div>
     )
   }
