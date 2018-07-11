26fe860a99c69db972480d094013edfd149f74f0
Mercedes Y Thompson <mythompson@bsu.edu>
2018-07-10 22:47:50 -0400

    add homework

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, NavLink, Switch } from 'react-router-dom'
 
 import './App.css'
 import Pokemon from './Pokemon'
+import Homework from './Homework'
 
 class App extends Component {
   render() {
@@ -12,13 +13,19 @@ class App extends Component {
           <h3>Ain't no party like an</h3>
           <h1>API Party</h1>
         </header>
+
         <ul className="navLinks">
           <li>
             <NavLink to="/pokemon">Pok&eacute; API</NavLink>
           </li>
+          <li>
+            <NavLink to="/homework">Homework</NavLink>
+          </li>
         </ul>
+
         <Switch>
           <Route path="/pokemon" component={Pokemon} />
+          <Route path="/homework" component={Homework} />
           <Route render={() => (
             <p>To get started, click one of the links above.</p>
           )}/>
@@ -27,4 +34,5 @@ class App extends Component {
     )
   }
 }
+
 export default App
